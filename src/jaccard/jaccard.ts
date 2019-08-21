928aad1363ca17ed35bca17f5feb5ef37912babb
rizentium <rizentium@gmail.com>
2019-08-21 11:34:54 +0700

    feat(jaccard-index): validator for NaN result

--- src/jaccard/jaccard.ts
+++ src/jaccard/jaccard.ts
@@ -1,16 +1,17 @@
 import Intersection from "../modules/intersection";
 import Union from "../modules/union";
 import Normalize from "../modules/normalize";
+import Validator from "../modules/validator";
 
 export function asText(from:string, to:string) {
   const normalized = Normalize(from, to);
   const intersection = Intersection(normalized.from, normalized.to);
   const union = Union(normalized.from, normalized.to, intersection);
-  return intersection.length/union.length;
+  return Validator(intersection.length,union.length) ? intersection.length/union.length : 0;
 }
 
 export function asArray(from:string[], to:string[]) {
   const intersection = Intersection(from, to);
   const union = Union(from, to, intersection);
-  return intersection.length/union.length;
+  return Validator(intersection.length,union.length) ? intersection.length/union.length : 0;
 }
---
+++ src/modules/validator.ts
@@ -0,0 +1,3 @@
+export default function Validator(x,y) {
+  return x === 1 && y === 0 ? false : x + y !== 0;
+}
